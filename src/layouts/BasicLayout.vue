caf35eca428a755799967094220b782e40e95d65
tutu <tutu@123.com>
2024-04-09 14:44:22 +0800

    1111

---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="basiclayout">
+    <a-layout style="height: 400px">
+      <a-layout-header class="header">
+        <GlobalHeader />
+      </a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer">
+        <a href="https://github.com/TUTUhappy" tutuOj> tutuOj </a>
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<style>
+#basiclayout {
+}
+
+#basiclayout .header {
+  margin-bottom: 16px;
+}
+
+#basiclayout .content {
+  background: linear-gradient(to right, #bbb, #fff);
+  margin-bottom: 16px;
+}
+
+#basiclayout .footer {
+  background: #efefef;
+  padding: 16px;
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  text-align: center;
+}
+</style>
+<script>
+import GlobalHeader from "@/components/GlobalHeader";
+
+export default {
+  components: { GlobalHeader },
+};
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<template>111</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+
+export default defineComponent({});
+</script>
